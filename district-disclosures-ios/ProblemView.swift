90d94fa89b94cc05d64e2514583fd621763cb9a7
MelTranfield2 <mrotranfield@gmail.com>
2022-02-12 17:38:06 +0000

    adding problem view, static images and dummy data

--- district-disclosures-ios/ProblemView.swift
+++ district-disclosures-ios/ProblemView.swift
@@ -5,4 +5,32 @@
 //  Created by Melissa Tranfield on 12/02/2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct ProblemView: View {
+    var body: some View {
+        ScrollView {
+        Image("tottenham-haringey")
+            .resizable()
+            .scaledToFit()
+        
+        Text("Knife Crime in Tottenham Hale")
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+        
+        Text("Haringey")
+                .font(.title)
+                .foregroundColor(.secondary)
+        
+        Text("The toilet is never being cleaned and I’m tired of having to always do the cleaning myself.")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProblemView()
+    }
+}
+
