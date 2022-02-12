f5a8b0d589c960c6b3198f73ea03b211cea41e68
MelTranfieldAda <melissa.tranfield@ada.ac.uk>
2022-02-12 16:00:48 +0000

    first commit - ContentView, locations.json (dummy data) and images

---
+++ district-disclosures-ios/district_disclosures_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  district_disclosures_iosApp.swift
+//  district-disclosures-ios
+//
+//  Created by Melissa Tranfield on 12/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct district_disclosures_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
